bd275c9f56b658e74424c53360dc75e7a5353dca
metasmile <cyrano905@gmail.com>
2015-11-02 02:50:05 +0900

    almost done

--- transync.py
+++ transync.py
@@ -96,7 +96,7 @@ base_dict = None
 translated_dict = {}
 
 # Get Base Language Specs
-walked = list(os.walk(expanduser(__RESOURCE_PATH__), topdown=True))
+walked = list(os.walk(__RESOURCE_PATH__, topdown=True))
 
 for dir, subdirs, files in walked:
     if os.path.basename(dir)==__BASE_RESOUCE_DIR__:
@@ -108,64 +108,106 @@ if not base_dict:
     print '[!] Not found "{0}" in target path "{1}"'.format(__BASE_RESOUCE_DIR__, __RESOURCE_PATH__)
     sys.exit(0)
 
+# core function
+def update_or_translate(target_file, lc):
+    base_content = base_dict[os.path.basename(target_file)]
+    base_kv = {}
+    for item in base_content:
+        base_kv[item['key']] = item['value']
+
+    target_kv = {}
+    for item in localizable.parse_strings(filename=target_file) if not notexist_or_empty_file(target_file) else None:
+        target_kv[item['key']] = item['value']
+
+    adding_keys = list(set(base_kv.keys()) - set(target_kv.keys()))
+    removing_keys = list(set(target_kv.keys()) - set(base_kv.keys()))
+    existing_keys = list(set(base_kv.keys()) - set(adding_keys) - set(removing_keys))
+
+    """
+    perform translate
+    """
+    translated_kv = {};
+    if len(adding_keys)>0:
+        print 'Translating... ', lc
+        translated_kv = dict(zip(adding_keys, translate_ms([base_kv[k] for k in adding_keys], lc)))
+
+    updated_content = []
+    for item in base_content:
+        k = item['key']
+        newitem = dict.fromkeys(item.keys())
+        newitem['key'] = k
+
+        #added
+        if k in adding_keys:
+            if k in translated_kv:
+                newitem['value'] = translated_kv[k]
+                newitem['comment'] = 'Translated from {0}'.format(base_kv[k])
+            else:
+                newitem['comment'] = 'Translate Failed'
+        #removed
+        if k in removing_keys:
+            continue
+        #existed
+        else:
+            newitem['value'] = base_kv[k]
+
+        updated_content.append(newitem)
+
+    return len(adding_keys)>0 or len(removing_keys)>0, updated_content, translated_kv
+
+def write_file(target_file, list_of_content):
+    f = codecs.open(target_file, "w", "utf-8")
+    contents = ''
+    for content in list_of_content:
+        if content['comment']:
+            contents += '/* {0} */'.format(content['comment']) + '\n'
+        contents += '"{0}" = "{1}";'.format(content['key'], content['value']) + '\n'
+    f.write(contents)
+    f.close()
+
+def create_file(target_file):
+    open(target_file, 'a').close()
+
+def notexist_or_empty_file(file):
+    os.path.getsize(file)==0 or os.path.exists(file)
+
 for dir, subdirs, files in walked:
     if dir.endswith((__DIR_SUFFIX__)):
         lc = os.path.basename(dir).split(__DIR_SUFFIX__)[0]
         if lc.find('_'): lc = lc.replace('_', __LANG_SEP__)
 
         if lc in __EXCLUDING_LANGS__:
+            print 'Skip: ', lc
             continue
+
+        lc = supported_lang(lc)
+        if not lc:
+            print 'Does not supported: ', lc
+            continue
+
+        print 'Start synchronizing... ', lc
 
         files = map(lambda f: f.decode('utf-8'), filter(lambda f: f.endswith(__FILE_SUFFIX__), files))
 
-        added_files = list(set(base_dict.keys()) - set(files))
-        removed_files = list(set(files) - set(base_dict.keys()))
-        existing_file = list(set(files) - set(added_files) - set(removed_files))
-
-        # print "Added:", added_files, "Removed:", removed_files, "Existing:", existing_file
+        added_files = [os.path.join(dir, f) for f in list(set(base_dict.keys()) - set(files))]
+        removed_files = [os.path.join(dir, f) for f in list(set(files) - set(base_dict.keys()))]
+        existing_file = [os.path.join(dir, f) for f in list(set(files) - set(added_files) - set(removed_files))]
 
         #remove - file
         for removed_file in removed_files:
             # print removed_file
-            os.rename(os.path.join(dir, removed_file), os.path.join(dir, removed_file+'.deleted'))
+            os.rename(removed_file, removed_file+'.deleted')
 
-        #update - file
         for ext_file in existing_file:
-            base_content = base_dict[ext_file]
-            base_keys = [key['key'] for key in base_content]
-
-            target_content = localizable.parse_strings(filename=os.path.join(dir, ext_file))
-            target_keys = [key['key'] for key in target_content]
-
-            adding_keys = list(set(base_keys) - set(target_keys))
-            removing_keys = list(set(target_keys) - set(base_keys))
-            existing_keys = list(set(base_keys) - set(adding_keys) - set(removing_keys))
-
-            # print translate_ms(['man', 'woman'], supported_lang('zh-Hans'))
-
-            updated_content = []
-            for item in base_content:
-                #new added
-                # updated_content.append({
-                #     'key':item['key']
-                #     'value': if item['key'] in target_keys
-                # })
-                continue
-
-            print lc, adding_keys, removing_keys
+            u, c, t = update_or_translate(ext_file, lc)
+            if u:
+                write_file(ext_file, c)
 
         #add - file
         for added_file in added_files:
-            base = base_dict[added_file]
-            # print os.path.join(dir, added_file)
+            create_file(added_file)
+            u, c, t = update_or_translate(added_file, lc)
+            if u:
+                write_file(added_file, c)
 
-
-# strings = localizable.parse_strings(filename='ar.lproj/Localizable.strings')
-# values = ''
-# for k in strings:
-#     values += "\"{0}\" = \"{1}\";\n".format(k['key'], k['value'])
-
-# path = os.path.join(expanduser('./'), 'ar.lproj/Localizable.strings')
-# f = codecs.open(path, "w", "utf-8")
-# f.write(values)
-# f.close()
+        print "Added:", added_files, "Removed:", removed_files, "Existing:", existing_file
