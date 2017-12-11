ddd23068a84b1bdf7e3284b1e6bebea3c2b362e9
metasmile <cyrano905@gmail.com>
2017-12-11 14:40:01 +0100

    format literal tests

--- strsync/strsync_playground.py
+++ strsync/strsync_playground.py
@@ -21,3 +21,33 @@ for l in ['zh-Hans', 'zh-CN', 'zh-SG'] + ['zh-Hant', 'zh-MO', 'zh-HK', 'zh-TW']:
     print strlocale.is_equal_lang_and_script(test_l1,l)
     print strlocale.is_equal_lang_and_script(test_l2,l)
     print '---'
+
+
+import re
+
+lines='''\
+Worker name is %s and id is %d
+That is %i%%
+%c
+Decimal: %d  Justified: %.6d
+%10c%5hc%5C%5lc
+The temp is %.*f
+%ss%lii
+%*.*s | %.3d | %lC | %s%%%02d'''
+
+cfmt='''\
+(                                  # start of capture group 1
+%                                  # literal "%"
+(?:                                # first option
+(?:[-+0 #]{0,5})                   # optional flags
+(?:\d+|\*)?                        # width
+(?:\.(?:\d+|\*))?                  # precision
+(?:h|l|ll|w|I|I32|I64)?            # size
+[cCdiouxXeEfgGaAnpsSZ]             # type
+) |                                # OR
+%%)                                # literal "%%"
+'''
+
+for line in lines.splitlines():
+    print '"{}"\n\t{}\n'.format(line,
+           tuple((m.start(1), m.group(1)) for m in re.finditer(cfmt, line, flags=re.X)))
